d30e24ac9c5c7679c69644d211ef0f8c8e080bf3
Ningchong Chen <nchen@zynga.com>
2015-04-27 14:56:48 -0700

    Added example project

--- BootcampExample/ViewController.swift
+++ BootcampExample/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
+    
+    var tipArray = [0.18, 0.20, 0.25]
+    var tipPerc = 0.18
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.text = "Tap On"
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        getTipSegment()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func didTapOnBillField(sender: UITextField) {
+        billField.text = ""
+    }
+    
+    @IBAction func didEditBillField(sender: UITextField) {
+        updateTotalAmount()
+    }
 
-
+    
+    
+    @IBAction func onTipSegmentChanged(sender: AnyObject) {
+        tipPerc = tipArray[tipSegment.selectedSegmentIndex]
+        updateTotalAmount()
+        setTipSegment()
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func updateTotalAmount() {
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPerc
+        var total = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    var tipIndexKey = "tipSegmentIndex"
+    func setTipSegment() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(tipSegment.selectedSegmentIndex, forKey: tipIndexKey)
+        defaults.synchronize()
+    }
+    
+    func getTipSegment() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var defaultTipSegmentIndex = 0
+        if (defaults.valueForKey(tipIndexKey) != nil) {
+            defaultTipSegmentIndex = defaults.integerForKey(tipIndexKey)
+        }
+        tipSegment.selectedSegmentIndex = defaultTipSegmentIndex
+        tipPerc = tipArray[tipSegment.selectedSegmentIndex]
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,13 @@
+# iOS Bootcamp Example
+
+This is an iOS example project to calculate the tip by typing in bill amount.
+
+## Contents
+
+1. How many hours did it take to complete?
+   2 hours
+2. Which required and optional stories have you completed?
+   a. Type in bill amount and get tip amount
+   b. Store and get default tip percentages
+3. GIF walkthrough of all required and optional stories
+   
